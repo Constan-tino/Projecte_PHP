f24d26096b7d239df4e2946963ae522c62a24d2c
Tino <cperezp39@alumnes.santjosepobrer.es>
2022-01-22 12:02:16 +0100

    Tino

--- Index.php
+++ Index.php
@@ -4,31 +4,9 @@
 <head>
     <!-- change to utf-8 if you use that -->
     <meta charset="iso-8859-1">
-    <title>Comandas</title>
+    <title>Pizarra</title>
 </head>
 <body>
-<form action="handel.php" method="post">
-    <h1>Ordenador de Comandas</h1>
-    <hr>
-    <label for="MESA">Seleciona la mesa en la que vas a hacer la comanda:</label>
-    <br>
-    <?php 
-        $mesas= array(1,2,3,4,5,6,7,8,9,10,11,12,13);
-        foreach($mesas as $mesa) {
-            echo "<input type=\"radio\" name=\"mesa\" value=\"$mesa\" required>$mesa";
-        }   
-    ?>
-    <hr>
-    <label >Numero de Comensales:</label>
-          <select  name="Comensales" >
-            <?php
-                for ($i=1;$i<=10;$i++) {
-                    echo "<option>$i</option>\n";
-                }
-            ?>
-          </select>
-    <hr>
-    <input type="submit" value="Siguiente >" class="boton">
-</form>
+<h1>Pizarra</h1>
 </body>
 </html>
