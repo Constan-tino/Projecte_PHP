e2d41d48ca44b17c0752b4d819ab0334dae0932c
Tino <cperezp39@alumnes.santjosepobrer.es>
2022-01-22 20:36:42 +0100

    Tino

--- kitchen.php
+++ kitchen.php
@@ -11,11 +11,29 @@ if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * from requests where Hecho = 1";
-if (mysqli_query($conn, $sql)) {
-    echo
+$sql = "SELECT * from requests where Hecho = 0";
+$result= mysqli_query($conn, $sql);
+$chequeo = mysqli_num_rows($result);
+echo "<h1>Comandas</h1>";
+echo "<div>";
+if ($chequeo>0) {
+    echo "<table><tr><th>Id</th><th>Mesa</th><th>Entrante</th><th>Bebida</th><th>Primer plato</th><th>Segundo plato</th><th>Postre</th><th>Hecho</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)){ 
+        //for loop better than this line
+        echo "<tr><td>".$row['Id']."</td><td>".$row['Mesa']."</td><td>".$row['Name']."</td><td>".$row['Entrante']."</td><td>".$row['Bebida']."</td><td>".$row['Primer plato']."</td><td>".$row['Segundo plato']."</td><td>".$row['Postre']."</td><td>"
+        echo "<input type=\"checkbox\" name=\"".$row['Id']."\" value=\"".$row['Id']."\">";
+        echo "</td></tr>";
+    }
+    echo "</table></div>";
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "No hay niguna petición de reserva :( <br>";
+}
+
+function updateTable($ids) {
+    for() {
+        $sql = "UPDATE requests SET Hecho = 1 WHERE Id = 'sadsad'";
+        $result= mysqli_query($conn, $sql);
+    }
 }
 
 mysqli_close($conn);
