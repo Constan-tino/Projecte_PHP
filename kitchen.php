06d94ffe562e314f34d0dcc2728c2a04380396f8
Tino <cperezp39@alumnes.santjosepobrer.es>
2022-01-22 19:08:23 +0100

    Tino

--- kitchen.php
+++ kitchen.php
@@ -3,7 +3,7 @@
 $servername = "bbdd.comeeahora.com";
 $username = "Tino";
 $database = "comeahora";
-$password = "";
+$password = "12345678Aa";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
@@ -11,15 +11,11 @@ if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
 
-for($i=0; $i<$n_people; $i++) {
-    $sql = "INSERT INTO requests (Id, Mesa, Hecho, Entrante, Primer plato, Segundo plato, Postre, Bebida) VALUES ($id, $mesa, 0, '$entrante[$i]', '$f_dish[$i]', '$s_dish[$i]', '$dessert[$i]', '$drinks[$i]')";
-    if (mysqli_query($conn, $sql)) {
-        echo "La comanda se a enviado correctamente";
-        sleep(5);
-        header('handel.php');
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+$sql = "SELECT * from requests where Hecho = 1";
+if (mysqli_query($conn, $sql)) {
+    echo
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
