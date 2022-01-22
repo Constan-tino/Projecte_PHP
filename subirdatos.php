f18cd0403af95bedc7fd020da7ad5f6b231c9803
Tino <cperezp39@alumnes.santjosepobrer.es>
2022-01-22 12:58:20 +0100

    Tino

--- subirdatos.php
+++ subirdatos.php
@@ -11,20 +11,40 @@ if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
 
-$n_people = $_POST['Comensales'];
+$n_people = $_POST['diners'];
+$mesa = $_POST['mesa'];
+$entrante = array();
 $f_dish = array();
 $s_dish = array();
 $dessert = array();
 $drinks = array();
 
-echo $n_people;
+for($i=1; $i<=$n_people; $i++){
+    array_push($entrante, $_POST["Entrante$i"])
+}
+
+for($i=1; $i<=$n_people; $i++){
+    array_push($f_dish, $_POST["Primer Plato$i"])
+}
+
+for($i=1; $i<=$n_people; $i++){
+    array_push($s_dish, $_POST["Segundo Plato$i"])
+}
+
+for($i=1; $i<=$n_people; $i++){
+    array_push($dessert, $_POST["Postre$i"])
+}
+
+for($i=1; $i<=$n_people; $i++){
+    array_push($drinks, $_POST["Bebida$i"])
+}
 
 for($i=0; $i<$n_people; $i++) {
-    $sql = "INSERT INTO pruebas1 (Id, Hecho, Entrante, Primer_Plato, Segundo_Plato, Postre, Bebida) VALUES ('', '', '', '$p_plato[$i]', '$s_plato[$i]', '$postre[$i]', '$bebida[$i]')";
+    $sql = "INSERT INTO pruebas1 (Hecho, Entrante, Primer_Plato, Segundo_Plato, Postre, Bebida) VALUES (0, '$entrante[$i]', '$f_dish[$i]', '$s_dish[$i]', '$dessert[$i]', '$drinks[$i]')";
     if (mysqli_query($conn, $sql)) {
         echo "La comanda se a enviado correctamente";
         sleep(5);
-        header('Index.php');
+        header('handel.php');
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
