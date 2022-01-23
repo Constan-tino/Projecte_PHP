5b63cae0bfe422e10250fd4a8a953329ab81f664
Tino <cperezp39@alumnes.santjosepobrer.es>
2022-01-23 12:58:18 +0100

    Tino

--- kitchen.php
+++ kitchen.php
@@ -1,4 +1,21 @@
 <?php
+    ini_set('display_errors', 1);
+                        error_reporting(E_ALL);
+                        session_start();
+                        //echo "Session started";
+    if (isset($_SESSION['chef'])) {
+?>
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- change to utf-8 if you use that -->
+    <meta charset="ISO-8859-1">
+    <title>Aplicación</title>
+    <link rel="stylesheet" href="handel.css">
+</head>
+<body>
+<?php
 
 $servername = "bbdd.comeeahora.com";
 $username = "Tino";
@@ -13,6 +30,8 @@ if (!$conn) {
 
 $sql = "SELECT * from requests where Hecho = 0";
 $id = "SELECT count(Id) from requests where Hecho = 0 group by Id";
+echo "<input type=\"number\" name=\"n_id\" value=\"$id\">";
+$contador = 1;
 $result= mysqli_query($conn, $sql);
 $chequeo = mysqli_num_rows($result);
 echo "<h1>Comandas</h1>";
@@ -22,30 +41,32 @@ if ($chequeo>0) {
     while ($row = mysqli_fetch_assoc($result)){ 
         //for loop better than this line
         echo "<tr><td>".$row['Id']."</td><td>".$row['Mesa']."</td><td>".$row['Name']."</td><td>".$row['Entrante']."</td><td>".$row['Bebida']."</td><td>".$row['Primer plato']."</td><td>".$row['Segundo plato']."</td><td>".$row['Postre']."</td><td>"
-        echo "<input type=\"checkbox\" name=\"c\" value=\"".$row['Id']."\">";
+        echo "<input type=\"checkbox\" name=\"c".$contador."\" value=\"".$row['Id']."\">";
         echo "</td></tr>";
+        $contador++;
     }
     echo "</table></div>";
 } else {
     echo "No hay niguna petición de reserva :( <br>";
 }
-
+mysqli_close($conn);
 
 if($_SERVER['REQUEST_METHOD']=="POST") {
-    for() {
-        $sql = "UPDATE requests SET Hecho = 1 WHERE Id = '$_POST['']'";
-        $result= mysqli_query($conn, $sql);
+
+    for($i=1; $i<=$_POST['n_id']; $i++) {
+        if(!empty($_POST["c$i"])) {
+            $sql = "UPDATE requests SET Hecho = 1 WHERE Id = '$_POST['c$i']'";
+            $result= mysqli_query($conn, $sql);
+        }
     }
     header('kitchen.php');
 }
 
-function updateTable($ids) {
-
+echo "</body></html>";
+sleep(30);
+header('Location: kitchen.php');
+}else {
+    header('Location: login.php');
+//echo "Logeate";
 }
-
-mysqli_close($conn);
-
-sleep(20);
-header('kitchen.php');
-
-?>
+    ?>
