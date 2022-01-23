75b38ef4013b88d5f595443bfdca50c6269fc746
xxava10 <97619010+xxava10@users.noreply.github.com>
2022-01-23 12:23:56 +0100

    xavi

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-session_start();
-//echo "Session started";
+//comprobamos que la session esta abierta
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
+    //echo "Session started";
 
     $login=array("waiter"=>"12345", "chef"=>"54321");
 
@@ -20,19 +20,25 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login comeahora</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-<form action="login.php" method="post">
-    <label>Nombre de usuario:</label><br>
-    <input type="text" name="username">
-<br><br>
-    <label>Contraseña:</label><br>
-    <input type="text" name="password">
-
-    <input type="submit" value="LogIn">
-</form>
-
+    <div class="login" >
+        <form action="login.php" method="post">
+            <h1>ComeAhora</h1>
+            <br>
+        <div class="users">    
+            <label>Nombre de usuario:</label><br>
+            <input type="text" name="username">
+            <br><br>
+            <label>Contraseña:</label><br>
+            <input type="text" name="password">
+            <input type="submit" value="LogIn">
+            <br>
+        </div>
+        </form>
+    </div>
 <?php 
 
 if($_SERVER['REQUEST_METHOD']=="POST") {
